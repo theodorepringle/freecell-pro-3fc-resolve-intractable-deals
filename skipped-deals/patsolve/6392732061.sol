efac479f0aafa6aa06e9c8df6c5094d2908b9c4c
theodorepringle <theodorepringle1@gmail.com>
2017-07-18 18:43:48 -0700

    Add deals

---
+++ skipped-deals/patsolve/6392732061.sol
@@ -0,0 +1,362 @@
+2S to temp
+QH to KC
+6H to temp
+JS to QH
+2H to temp
+6H to 7S
+JC to temp
+JS to QD
+QH to KS
+JC to QH
+JS to temp
+QD to KC
+AD out
+JS to QD
+3D to temp
+8S to 9H
+4S to 5H
+8S to 9D
+3D to 4S
+6H to temp
+8S to 9H
+7S to 8D
+AC out
+2D out
+TD to JC
+6H to 7S
+TD to temp
+8C to 9D
+TD to JS
+4D to temp
+TH to JC
+5C to 6D
+4D to 5S
+3D to temp
+4D to 5C
+5S to 6H
+4D to 5S
+4S to 5D
+3D to 4S
+4D to 5C
+5H to temp
+7D to 8S
+2H to 3C
+TH to temp
+7D to 8C
+4D to 5S
+2S to 3D
+TD to temp
+4D to 5C
+7D to 8S
+TH to JS
+TD to JC
+5S to temp
+7D to 8C
+8S to temp
+5S to 6H
+9H to temp
+5H to 6S
+2H to temp
+4D to 5S
+3C to 4D
+2H to 3C
+TH to temp
+TD to JS
+TH to JC
+5H to temp
+6S to 7D
+5H to 6C
+KH to temp
+9H to empty pile
+TD to temp
+5H to 6S
+TH to JS
+8S to 9H
+TD to JC
+5H to 6C
+6S to temp
+2S to temp
+7D to 8S
+6S to 7D
+3D to temp
+4S to 5H
+5D to 6S
+2C out
+4S to 5D
+3D to 4S
+TH to temp
+KH to empty pile
+2S to 3D
+5H to temp
+2H to temp
+TH to JS
+TD to temp
+5H to 6C
+TH to JC
+2S to temp
+TD to JS
+KH to temp
+2S to empty pile
+3D to temp
+4S to 5H
+3D to 4S
+3C to temp
+4D to 5C
+3C to 4D
+5S to temp
+2S to 3D
+2H to 3C
+5S to empty pile
+TD to temp
+JS to temp
+5S to 6H
+KH to empty pile
+JS to QD
+2S to temp
+3D to temp
+TD to JS
+4S to 5D
+3D to 4S
+5S to temp
+2S to 3D
+TH to temp
+TD to temp
+TH to JS
+JC to temp
+5S to 6H
+TH to temp
+JS to QH
+JC to QD
+2S to temp
+TH to JC
+3D to temp
+TH to JS
+4S to 5H
+3D to 4S
+JC to temp
+2S to 3D
+5D to temp
+JC to QD
+TD to JC
+6S to temp
+2S to temp
+7D to 8C
+6S to 7D
+3D to temp
+5D to 6S
+2H to temp
+3D to 4S
+3C to temp
+2S to 3D
+KH to temp
+2H to empty pile
+3C to 4D
+2S to temp
+3D to temp
+4S to 5D
+3D to 4S
+TD to temp
+2S to 3D
+8S to temp
+TD to JC
+2H to temp
+3C to empty pile
+2H to 3C
+TH to temp
+TD to JS
+8S to 9H
+2S to temp
+TH to JC
+3D to temp
+4S to 5H
+3D to 4S
+2H to temp
+2S to 3D
+TD to temp
+3C to 4D
+2H to 3C
+TH to temp
+JC to empty pile
+JS to QD
+TD to JC
+2S to temp
+TH to JS
+3D to temp
+4S to 5D
+3D to 4S
+2S to 3D
+TD to temp
+JC to QH
+2S to temp
+3D to empty pile
+2S to 3D
+5H to temp
+TH to JC
+TD to JS
+2S to temp
+3D to 4S
+5H to empty pile
+3D to temp
+4S to 5H
+3D to 4S
+5S to temp
+2S to 3D
+TH to temp
+TD to JC
+5D to 6C
+5S to 6H
+2S to temp
+5D to 6S
+TH to JS
+3D to temp
+5D to 6C
+4S to 5D
+3D to 4S
+5H to 6S
+2S to empty pile
+3D to temp
+4S to 5H
+3D to 4S
+TH to temp
+2S to temp
+3D to empty pile
+TD to JS
+4S to 5D
+2S to 3D
+TD to temp
+4S to 5H
+TH to JS
+5D to temp
+TH to JC
+TD to JS
+2S to temp
+3D to 4S
+5D to empty pile
+3D to temp
+4S to 5D
+3D to 4S
+TD to temp
+5H to 6C
+2S to 3D
+TD to JS
+6S to temp
+7D to 8S
+TD to temp
+6S to 7D
+2S to temp
+TD to JS
+3D to temp
+5H to 6S
+4S to 5H
+3D to 4S
+TD to temp
+5D to 6C
+TD to empty pile
+5S to temp
+TH to JS
+TD to JC
+2S to empty pile
+3D to temp
+4S to 5D
+3D to 4S
+6H to temp
+2S to 3D
+7S to empty pile
+6H to 7S
+TH to temp
+TD to JS
+5S to 6H
+2S to temp
+TD to JC
+TH to JS
+3D to temp
+4S to 5H
+3D to 4S
+TH to temp
+2S to 3D
+8C to temp
+TH to JS
+9D to temp
+4H to 5S
+9D to TC
+8D to temp
+8C to 9D
+4C to 5D
+7H to 8C
+QS to temp
+3S to 4H
+AH out
+2H out
+3C out
+4C out
+KH to empty pile
+2S to temp
+3D out
+4D out
+5D out
+6C to 7H
+QS to KH
+JH to QS
+2S to 3H
+JH to temp
+6C to temp
+5C out
+JH to QS
+3S to temp
+6C to 7H
+6D to temp
+6C out
+7C out
+6D to empty pile
+3S to 4H
+JH to temp
+TD to temp
+TH to JC
+6D out
+JH to QS
+JS to temp
+QD to empty pile
+JS to QD
+KC to temp
+TD to JS
+KD to temp
+AS out
+2S out
+3H out
+3S out
+4S out
+4H out
+5H out
+5S out
+6S out
+7D out
+6H out
+7H out
+8H out
+8C out
+7S out
+8S out
+9H out
+TH out
+8D out
+9D out
+TD out
+JH out
+QC to temp
+9C out
+TC out
+9S out
+JC out
+QC out
+KC out
+QH to temp
+KS to temp
+TS out
+JS out
+JD out
+QD out
+QS out
+QH out
+KH out
+KS out
+KD out
