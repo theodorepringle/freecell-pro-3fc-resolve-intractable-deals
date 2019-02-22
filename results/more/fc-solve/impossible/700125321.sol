fd02665430f7e1616a077a6f6440973bbbf8d2f1
theodorepringle <theodorepringle1@gmail.com>
2019-02-22 03:20:19 -0800

    remove

--- results/more/fc-solve/impossible/700125321.sol
+++
@@ -1,3 +0,0 @@
-I could not solve this game.
-Total number of states checked is 40491477.
-This scan generated 40491476 states.
--- results/more/fc-solve/impossible/700254425.sol
+++
@@ -1,3 +0,0 @@
-I could not solve this game.
-Total number of states checked is 27041789.
-This scan generated 27041789 states.
